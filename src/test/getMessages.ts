39a24857443831c11f7b884d55132f33e8b957fd
Dr. Jochen Manns <manns@insynergie.de>
2019-07-28 20:43:04 +0200

    code reorg

--- src/test/getMessages.ts
+++ src/test/getMessages.ts
@@ -1,4 +1,4 @@
-import { getMessage } from '../src/getMessage'
+import { getMessage } from '../getMessage'
 
 export function testGetMessage(): void {
     console.log(getMessage(new Error('[TBD choose a test framework]')))
